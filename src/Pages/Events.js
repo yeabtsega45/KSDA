adca06ddb4b5a2ea3d785f3ec99adeea01feb9ee
yeabtsega45 <yabtsega2022@gmail.com>
2023-01-18 10:30:13 +0300

    adding css to events

--- src/Pages/Events.js
+++ src/Pages/Events.js
@@ -2,21 +2,41 @@ import React from "react";
 import "./Events.css";
 import img1 from "../Assets/frontpage.jpg";
 import Card from "../Components/Card";
-import img2 from "./Assets/.jpg";
-import img3 from "./Assets/.jpg";
-import img4 from "./Assets/.jpg";
-import img5 from "./Assets/.jpg";
+import event_img1 from "../Assets/event1.jpg";
+import event_img2 from "../Assets/event2.jpg";
+import event_img3 from "../Assets/event3.jpg";
+import event_img4 from "../Assets/event4.jpg";
 
 function Events() {
   return (
     <div className="events">
       <img src={img1} alt="event_image" />
       <h1>Upcoming Events</h1>
-      <div className="cards">
-        <Card image={img2} title="" date="" description="" />
-        <Card image={img3} title="" date="" description="" />
-        <Card image={img4} title="" date="" description="" />
-        <Card image={img5} title="" date="" description="" />
+      <div className="event_cards">
+        <Card
+          image={event_img1}
+          title="kerestena serg ena melash"
+          date="tahesas 27-29 2015"
+          description="ye tselot ena ye ameleko gize"
+        />
+        <Card
+          image={event_img2}
+          title="nu abren getan enamelek"
+          date="hidar 24, keseat 11:30"
+          description="ye ameleko meshet"
+        />
+        <Card
+          image={event_img3}
+          title="ye neseha ena ye seryet program"
+          date="pagume 4-5, 2014"
+          description="yeteleyayu tegabaz mezemran, ye kal gize, ye tselot saten, yemayfelegewen maseweged"
+        />
+        <Card
+          image={event_img4}
+          title="yeh new tarike"
+          date="hamele 15/2015, arb meshet 11:30-1:00"
+          description="leyu yehone mesekerenet ke abel gar"
+        />
       </div>
     </div>
   );
