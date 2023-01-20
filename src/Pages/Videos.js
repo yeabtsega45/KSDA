e9c58e8f00aa18b7f95f8511a91c52c491c1ce7c
yeabtsega45 <yabtsega2022@gmail.com>
2023-01-20 11:39:36 +0300

    adding links to the card component

--- src/Pages/Videos.js
+++ src/Pages/Videos.js
@@ -15,30 +15,42 @@ function Videos() {
       <img src={img1} alt="header_image" />
       <h1>Uploaded Videos</h1>
       <div className="cards_vertical">
-        <CardVertical
-          image={video1}
-          title="ክርስትና ሠርግና ምላሽ - ቀን 03 - ፓስተር አብርሃም ተካ"
-        />
-        <CardVertical
-          image={video2}
-          title="ክርስትና ሠርግና ምላሽ - ቀን 02 - ፓስተር አብርሃም ተካ"
-        />
-        <CardVertical
-          image={video3}
-          title="ክርስትና ሠርግና ምላሽ - ቀን 01 - ፓስተር አብርሃም ተካ"
-        />
-        <CardVertical
-          image={video4}
-          title="New song - ፍቅር ነህ - Fiker Neh - የኮተቤ ሰባተኛዉ ቀን አድቬንቴስት ቤተክርስቲያን መዘምራን"
-        />
-        <CardVertical
-          image={video5}
-          title="ጓዳችን (guadachen) new standard short movies"
-        />
-        <CardVertical
-          image={video6}
-          title="ልዩ የሆነ የአምልኮ ምሽት በኮተቤ 7ኛ ቀን አድቬንቲስት ቤተክርስቲያን"
-        />
+        <a href="https://www.youtube.com/watch?v=6zS1QYcAi6c&t=2s">
+          <CardVertical
+            image={video1}
+            title="ክርስትና ሠርግና ምላሽ - ቀን 03 - ፓስተር አብርሃም ተካ"
+          />
+        </a>
+        <a href="https://www.youtube.com/watch?v=SPcp4ejxSBA&t=1s">
+          <CardVertical
+            image={video2}
+            title="ክርስትና ሠርግና ምላሽ - ቀን 02 - ፓስተር አብርሃም ተካ"
+          />
+        </a>
+        <a href="https://www.youtube.com/watch?v=5Ax128q0cZc">
+          <CardVertical
+            image={video3}
+            title="ክርስትና ሠርግና ምላሽ - ቀን 01 - ፓስተር አብርሃም ተካ"
+          />
+        </a>
+        <a href="https://www.youtube.com/watch?v=LhjXa-eF1ro">
+          <CardVertical
+            image={video4}
+            title="New song - ፍቅር ነህ - Fiker Neh - የኮተቤ ሰባተኛዉ ቀን አድቬንቴስት ቤተክርስቲያን መዘምራን"
+          />
+        </a>
+        <a href="https://www.youtube.com/watch?v=K9cZnroi6Lg">
+          <CardVertical
+            image={video5}
+            title="ጓዳችን (guadachen) new standard short movies"
+          />
+        </a>
+        <a href="https://www.youtube.com/watch?v=me_M54g-6fI&t=2s">
+          <CardVertical
+            image={video6}
+            title="ልዩ የሆነ የአምልኮ ምሽት በኮተቤ 7ኛ ቀን አድቬንቲስት ቤተክርስቲያን"
+          />
+        </a>
       </div>
     </div>
   );
