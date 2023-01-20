a3fbc6dc04eb34e82803690d31ae4acd61bc0f9a
yeabtsega45 <yabtsega2022@gmail.com>
2023-01-20 10:24:47 +0300

    adding icon overlaying image

--- src/Components/Card_Vertical.js
+++ src/Components/Card_Vertical.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./Card_Vertical.css";
+import YouTubeIcon from "@material-ui/icons/YouTube";
 
 function CardVertical({ image, title }) {
   return (
     <div className="card_vertical">
       <img src={image} className="cardvertical_image" alt="images" />
+      <YouTubeIcon className="card_icon" />
       <div>
         <h3>{title}</h3>
       </div>
