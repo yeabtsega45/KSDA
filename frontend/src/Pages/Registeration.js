75a1f3b8de20feeacc3d98e23b689bcea8e08b2c
yeabtsega45 <yabtsega2022@gmail.com>
2024-05-20 19:59:43 +0300

    add registration form

--- frontend/src/Pages/Registeration.js
+++ frontend/src/Pages/Registeration.js
@@ -9,7 +9,7 @@ function Registeration() {
     email: "",
     password: "",
     address: "",
-    salary: "",
+    gender: "",
     image: "",
   });
   const navigate = useNavigate();
@@ -21,7 +21,7 @@ function Registeration() {
     formdata.append("email", data.email);
     formdata.append("password", data.password);
     formdata.append("address", data.address);
-    formdata.append("salary", data.salary);
+    formdata.append("gender", data.gender);
     formdata.append("image", data.image);
     axios
       .post("/create", formdata)
@@ -33,41 +33,85 @@ function Registeration() {
   };
   return (
     <div className="flex flex-col items-center pt-4">
-      <h2>Add Employee</h2>
-      <form className="w-full max-w-lg" onSubmit={handleSubmit}>
-        <div className="sm:col-span-3">
+      <h2>የኮተቤ አድቬንቲስት ቤተክርስትያን የአባልነት መመዝገቢያ ቅጽ </h2>
+      <form className="w-full max-w-lg px-5" onSubmit={handleSubmit}>
+        <div className="flex">
+          <div className="sm:col-span-3 my-2 mr-2">
+            <label
+              htmlFor="inputName"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              ስም
+            </label>
+            <input
+              type="text"
+              className="block w-full rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              id="inputName"
+              placeholder="Enter Name"
+              autoComplete="off"
+              onChange={(e) => setData({ ...data, name: e.target.value })}
+            />
+          </div>
+          <div className="sm:col-span-3 my-2">
+            <label
+              htmlFor="inputName"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              የአባት ስም
+            </label>
+            <input
+              type="text"
+              className="block w-full rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              id="inputName"
+              placeholder="Enter Name"
+              autoComplete="off"
+              onChange={(e) => setData({ ...data, name: e.target.value })}
+            />
+          </div>
+        </div>
+        <div className="sm:col-span-3 my-2">
           <label
-            htmlFor="inputName"
+            htmlFor="inputGender"
             className="block text-sm font-medium leading-6 text-gray-900"
           >
-            Name
+            ጾታ
           </label>
           <input
-            type="text"
-            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 mt-2"
-            id="inputName"
-            placeholder="Enter Name"
-            autoComplete="off"
-            onChange={(e) => setData({ ...data, name: e.target.value })}
+            type="radio"
+            className="text-sm font-medium leading-6"
+            id="inputGender"
+            name="gender"
+            value="Male"
+            onChange={(e) => setData({ ...data, gender: e.target.value })}
           />
+          ወንድ
+          <input
+            type="radio"
+            className="text-sm font-medium leading-6 mb-5"
+            id="inputGender"
+            name="gender"
+            value="Female"
+            onChange={(e) => setData({ ...data, gender: e.target.value })}
+          />
+          ሴት
         </div>
-        <div className="sm:col-span-3">
+        <div className="sm:col-span-3 my-2">
           <label
             htmlFor="inputEmail4"
             className="block text-sm font-medium leading-6 text-gray-900"
           >
-            Email
+            ኢሜል አድራሻ
           </label>
           <input
             type="email"
-            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 mt-2"
+            className="block w-full rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
             id="inputEmail4"
             placeholder="Enter Email"
             autoComplete="off"
             onChange={(e) => setData({ ...data, email: e.target.value })}
           />
         </div>
-        <div className="sm:col-span-3">
+        <div className="sm:col-span-3 my-2">
           <label
             htmlFor="inputPassword4"
             className="block text-sm font-medium leading-6 text-gray-900"
@@ -76,29 +120,13 @@ function Registeration() {
           </label>
           <input
             type="password"
-            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 mt-2"
+            className="block w-full rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
             id="inputPassword4"
             placeholder="Enter Password"
             onChange={(e) => setData({ ...data, password: e.target.value })}
           />
         </div>
-        <div className="sm:col-span-3">
-          <label
-            htmlFor="inputSalary"
-            className="block text-sm font-medium leading-6 text-gray-900"
-          >
-            Salary
-          </label>
-          <input
-            type="text"
-            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 mt-2"
-            id="inputSalary"
-            placeholder="Enter Salary"
-            autoComplete="off"
-            onChange={(e) => setData({ ...data, salary: e.target.value })}
-          />
-        </div>
-        <div className="sm:col-span-3">
+        <div className="sm:col-span-3 my-2">
           <label
             htmlFor="inputAddress"
             className="block text-sm font-medium leading-6 text-gray-900"
@@ -107,14 +135,14 @@ function Registeration() {
           </label>
           <input
             type="text"
-            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 mt-2"
+            className="block w-full rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
             id="inputAddress"
             placeholder="1234 Main St"
             autoComplete="off"
             onChange={(e) => setData({ ...data, address: e.target.value })}
           />
         </div>
-        <div className="col-span-full">
+        <div className="col-span-full my-2">
           <label
             className="block text-sm font-medium leading-6 text-gray-900"
             htmlFor="inputGroupFile01"
@@ -123,12 +151,12 @@ function Registeration() {
           </label>
           <input
             type="file"
-            className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10"
+            className="flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10"
             id="inputGroupFile01"
             onChange={(e) => setData({ ...data, image: e.target.files[0] })}
           />
         </div>
-        <div className="mt-6 flex items-center justify-end gap-x-6">
+        <div className="my-2 flex items-center justify-end gap-x-6">
           <button
             type="submit"
             className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
