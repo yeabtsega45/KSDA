514674a9b2468954920050d022737efeb1ec81d0
yeabtsega45 <yabtsega2022@gmail.com>
2024-05-21 00:04:33 +0300

    refactor video data

--- frontend/src/Pages/Videos.js
+++ frontend/src/Pages/Videos.js
@@ -1,13 +1,8 @@
 import React from "react";
 import img1 from "../Assets/frontpage.jpg";
 import "./Videos.css";
-import CardVertical from "../Components/Card_Vertical";
-import video1 from "../Assets/video1.png";
-import video2 from "../Assets/video2.png";
-import video3 from "../Assets/video3.png";
-import video4 from "../Assets/video4.png";
-import video5 from "../Assets/video5.png";
-import video6 from "../Assets/video6.png";
+import Box from "../Components/Box";
+import VideoData from "../Utils/VideoData";
 
 function Videos() {
   return (
@@ -15,42 +10,11 @@ function Videos() {
       <img src={img1} alt="header_image" />
       <h1>Uploaded Videos</h1>
       <div className="cards_vertical">
-        <a href="https://www.youtube.com/watch?v=6zS1QYcAi6c&t=2s">
-          <CardVertical
-            image={video1}
-            title="ክርስትና ሠርግና ምላሽ - ቀን 03 - ፓስተር አብርሃም ተካ"
-          />
-        </a>
-        <a href="https://www.youtube.com/watch?v=SPcp4ejxSBA&t=1s">
-          <CardVertical
-            image={video2}
-            title="ክርስትና ሠርግና ምላሽ - ቀን 02 - ፓስተር አብርሃም ተካ"
-          />
-        </a>
-        <a href="https://www.youtube.com/watch?v=5Ax128q0cZc">
-          <CardVertical
-            image={video3}
-            title="ክርስትና ሠርግና ምላሽ - ቀን 01 - ፓስተር አብርሃም ተካ"
-          />
-        </a>
-        <a href="https://www.youtube.com/watch?v=LhjXa-eF1ro">
-          <CardVertical
-            image={video4}
-            title="New song - ፍቅር ነህ - Fiker Neh - የኮተቤ ሰባተኛዉ ቀን አድቬንቴስት ቤተክርስቲያን መዘምራን"
-          />
-        </a>
-        <a href="https://www.youtube.com/watch?v=K9cZnroi6Lg">
-          <CardVertical
-            image={video5}
-            title="ጓዳችን (guadachen) new standard short movies"
-          />
-        </a>
-        <a href="https://www.youtube.com/watch?v=me_M54g-6fI&t=2s">
-          <CardVertical
-            image={video6}
-            title="ልዩ የሆነ የአምልኮ ምሽት በኮተቤ 7ኛ ቀን አድቬንቲስት ቤተክርስቲያን"
-          />
-        </a>
+        {VideoData.map((video) => (
+          <a key={video.id} href={video.url}>
+            <Box image={video.image} title={video.title} />
+          </a>
+        ))}
       </div>
     </div>
   );
---
+++ frontend/src/Utils/VideoData.js
@@ -0,0 +1,48 @@
+import video1 from "../Assets/video1.png";
+import video2 from "../Assets/video2.png";
+import video3 from "../Assets/video3.png";
+import video4 from "../Assets/video4.png";
+import video5 from "../Assets/video5.png";
+import video6 from "../Assets/video6.png";
+
+const VideoData = [
+  {
+    id: 1,
+    image: video1,
+    title: "ክርስትና ሠርግና ምላሽ - ቀን 03 - ፓስተር አብርሃም ተካ",
+    url: "https://www.youtube.com/watch?v=6zS1QYcAi6c&t=2s",
+  },
+  {
+    id: 2,
+    image: video2,
+    title: "ክርስትና ሠርግና ምላሽ - ቀን 02 - ፓስተር አብርሃም ተካ",
+    url: "https://www.youtube.com/watch?v=SPcp4ejxSBA&t=1s",
+  },
+  {
+    id: 3,
+    image: video3,
+    title: "ክርስትና ሠርግና ምላሽ - ቀን 01 - ፓስተር አብርሃም ተካ",
+    url: "https://www.youtube.com/watch?v=5Ax128q0cZc",
+  },
+  {
+    id: 4,
+    image: video4,
+    title:
+      "New song - ፍቅር ነህ - Fiker Neh - የኮተቤ ሰባተኛዉ ቀን አድቬንቴስት ቤተክርስቲያን መዘምራን",
+    url: "https://www.youtube.com/watch?v=LhjXa-eF1ro",
+  },
+  {
+    id: 5,
+    image: video5,
+    title: "ጓዳችን (guadachen) new standard short movies",
+    url: "https://www.youtube.com/watch?v=K9cZnroi6Lg",
+  },
+  {
+    id: 6,
+    image: video6,
+    title: "ልዩ የሆነ የአምልኮ ምሽት በኮተቤ 7ኛ ቀን አድቬንቲስት ቤተክርስቲያን",
+    url: "https://www.youtube.com/watch?v=me_M54g-6fI&t=2s",
+  },
+];
+
+export default VideoData;
