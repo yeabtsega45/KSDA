ddc481eec9c7f44590a97f979fb7a9a6308465c7
yeabtsega45 <yabtsega2022@gmail.com>
2024-05-20 22:02:10 +0300

    update card component

--- frontend/src/Pages/Events.js
+++ frontend/src/Pages/Events.js
@@ -1,11 +1,8 @@
 import React from "react";
 import "./Events.css";
 import img1 from "../Assets/frontpage.jpg";
-import Card from "../Components/Card";
-import event_img1 from "../Assets/event1.jpeg";
-import event_img2 from "../Assets/event2.jpeg";
-import event_img3 from "../Assets/event3.jpeg";
-import event_img4 from "../Assets/event4.jpeg";
+import EventData from "../Utils/EventData";
+import Box from "../Components/Box";
 
 function Events() {
   return (
@@ -20,30 +17,15 @@ function Events() {
       />
       <h1>Upcoming Events</h1>
       <div className="event_cards">
-        <Card
-          image={event_img1}
-          title="ክርስትና ፣ ሰርግ እና ምላሽ"
-          date="ታህሳስ 27-29 2015"
-          description="የፀሎት እና የአምልኮ ጊዜ"
-        />
-        <Card
-          image={event_img2}
-          title="ኑ አብረን ጌታን እናምልክ"
-          date="ህዳር 24 ፣ ከሰዐት 11፡30"
-          description="የአምልኮ ምሽት"
-        />
-        <Card
-          image={event_img3}
-          title="የንሰሃ እና የስርየት ፕሮግራም"
-          date="ጳጉሜ 4-5 2014"
-          description="የተለያዩ ተጋባዥ መዘምራን ፣ የቃል ጊዜ ፣ የፀሎት ሳጥን ፣ የማይፈለገውን ማስወገድ"
-        />
-        <Card
-          image={event_img4}
-          title="ይህ ነው ታሪኬ"
-          date="ሀምሌ 15/2015 ፣ አርብ ምሽት 11፡30-1፡00"
-          description="የተለየ ምስክርነት ከአቤል ጋር"
-        />
+        {EventData.map((event) => (
+          <Box
+            key={event.id}
+            image={event.image}
+            title={event.title}
+            date={event.date}
+            description={event.description}
+          />
+        ))}
       </div>
     </div>
   );
