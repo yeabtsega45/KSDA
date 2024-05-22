c9d33853b7632acfc8be6d704d5b39aa769c0f66
yeabtsega45 <yabtsega2022@gmail.com>
2024-05-22 17:54:34 +0300

    add membership page

--- frontend/src/Pages/Members.js
+++ frontend/src/Pages/Members.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Hero from "../Components/Hero";
+import "./Members.css";
 
 function Members() {
   return (
     <div>
       <Hero title="Membership" />
+      <div className="no-members">
+        <h3 className="no-members-text">No members yet</h3>
+      </div>
     </div>
   );
 }
